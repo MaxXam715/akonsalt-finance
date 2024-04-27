e2e18bf24e470480c35da57f1ae446f439416b4a
Max <gorbatenkomax@yandex.ru>
2024-04-27 22:26:33 +0300

    update

--- backend/template.php
+++ backend/template.php
@@ -5,7 +5,7 @@ $version_new = "";
 $root = $_SERVER['DOCUMENT_ROOT'];
 require_once $root . '/backend/clearCash.php';
 
-if ($_SERVER['HTTP_HOST'] != 'andranik1.beget.tech') {
+if ($_SERVER['HTTP_HOST'] != 'you-finances.ru') {
     $version = mt_rand(10000, 99999999);
 } else {
     $version = "1.0.189";
