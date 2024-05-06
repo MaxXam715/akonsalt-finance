ae3ad2a3ce63070e2ea511f58735238e0dac2c41
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-05-06 15:11:44 +0300

    update

--- backend/template.php
+++ backend/template.php
@@ -8,7 +8,7 @@ require_once $root . '/backend/clearCash.php';
 if ($_SERVER['HTTP_HOST'] != 'you-finances.ru') {
     $version = mt_rand(10000, 99999999);
 } else {
-    $version = "1.0.114";
+    $version = "1.0.115";
 
     clearCash($root . "/assets", $version);
     clearCash($root . "/css", $version);
