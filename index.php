36e7476e843522da5e1a869abae4ae888d3898b1
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-05-06 11:57:04 +0300

    update

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $versionJSON = file_get_contents($root . "/static/version.txt");
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <title>You Finances</title>
+    <title>You Finances 2</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="version" content="<?= $version ?>">
