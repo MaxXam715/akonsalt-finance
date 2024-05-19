d6520209884ab5269722e49c176055a740ffaae2
Max <gorbatenkomax@yandex.ru>
2024-05-19 11:31:33 +0300

    update

--- backend/template.php
+++ backend/template.php
@@ -8,12 +8,22 @@ require_once $root . '/backend/clearCash.php';
 if ($_SERVER['HTTP_HOST'] != 'you-finances.ru') {
     $version = mt_rand(10000, 99999999);
 } else {
-    $version = "1.0.119";
+    $getFileVersion = file($root."/backend/version.txt", FILE_IGNORE_NEW_LINES);
+    $version_now = $getFileVersion[0];
+    $version_new = $getFileVersion[1];
+    $version = $version_now;
 
-    clearCash($root . "/assets", $version);
-    clearCash($root . "/css", $version);
-    clearCash($root . "/js", $version);
-    clearCash($root . "/components", $version);
-    clearCash($root . "/pages", $version);
-    clearCash($root . "/plugins/modal", $version);
+    if ($version_now != $version_new) {
+        $version_now = $version_new;
+        $version = $version_now;
+        $getFileVersion[0] = $getFileVersion[1];
+        file_put_contents($root."/backend/version.txt", implode(PHP_EOL, $getFileVersion));
+
+        clearCash($root . "/assets", $version);
+        clearCash($root . "/css", $version);
+        clearCash($root . "/js", $version);
+        clearCash($root . "/components", $version);
+        clearCash($root . "/pages", $version);
+        clearCash($root . "/plugins/modal", $version);
+    }
 }
