109bcb8c2028fb3c044a379455ee62624ada2432
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-05-29 11:59:10 +0300

    update

--- index.php
+++ index.php
@@ -38,12 +38,16 @@ $versionJSON = file_get_contents($root . "/static/version.txt");
     <link rel="image_src" href="/assets/img/logo-banner.jpg">
 
     <!-- Favicons -->
-<!--    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">-->
-<!--    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">-->
-<!--    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">-->
-<!--    <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#5bbad5">-->
-<!--    <meta name="msapplication-TileColor" content="#F7F1E4">-->
-<!--    <meta name="theme-color" content="#F7F1E4">-->
+    <link rel="icon" type="image/x-icon" href="/static/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="/static/favicon/favicon-48x48.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/static/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="128x128" href="/static/favicon/favicon-128x128.png">
+    <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#000000">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="theme-color" content="#000000">
 
     <!-- Style -->
     <link rel="stylesheet" href="/css/global.css?v=<?= $version ?>">
