1f2a6e74427ae159e7f27891986955caf4921007
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-08-29 11:40:05 +0300

    update

--- js/sendByEmail.js
+++ js/sendByEmail.js
@@ -1,3 +1,9 @@
 export default function sendByEmail() {
+    var sendMail = XMLHttpRequestAJAX({
+        url: `/backend/sendMail.php`,
+        method: "POST",
+        body: {}
+    });
 
+    console.log('sendMail', sendMail)
 }
