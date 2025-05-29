a77a4dcf230abdb4aa1ea3f0c88d1caee4117bab
Max <gorbatenkomax@yandex.ru>
2025-05-29 23:54:46 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 const version = document.querySelector("head meta[name='version']").getAttribute("content");
+console.log('version', version)
 const global_phone = "+79362894030";
 const global_email = "official@akonsalt-finance.ru";
 const legal_IP = "Кешишян Андраник Саркисович";
